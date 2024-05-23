3d0285e93202ee22770f1c20aa360c958145942d
Karthik Reddy Banda <147733716+bandakarthikreddy@users.noreply.github.com>
2024-05-23 22:02:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-customhost = "my-project.cds0cw4ealo3.us-east-1.rds.amazonaws.com"
+customhost = "employee.cds0cw4ealo3.us-east-1.rds.amazonaws.com"
 customuser = "admin"
-custompass = "password123"
+custompass = "password"
 customdb = "employee"
 custombucket = "karthik1997"
 customregion = "us-east-1"
